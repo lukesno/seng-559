871398eaf183fcfd99b57417bce7551e802299e0
Tyler Tran <tylertran1017@gmail.com>
2024-03-03 03:04:59 -0700

    Minor syntax for LB

--- backend/load-balancer/central.js
+++ backend/load-balancer/central.js
@@ -3,28 +3,28 @@ const http = require("http");
 // List of backend servers
 const backendServers = [
   { host: "localhost", port: 3001 },
-  { host: "localhost", port: 3002 },
+  // { host: "localhost", port: 3002 },
 ];
 
 let currentBackendIndex = 0;
 
 const server = http.createServer((req, res) => {
-    const { host, port } = backendServers[currentBackendIndex];
-    const requestOptions = {
-        hostname: host,
-        port: port,
-        path: req.url,
-        method: req.method,
-        headers: req.headers
-    };
-    // Log incoming request details
-    console.log(`Method: ${req.method}`);
-    console.log(`URL: ${req.url}`);
-    console.log(`Headers: ${JSON.stringify(req.headers)}`);
-    console.log('Payload:', req.body);
+  const { host, port } = backendServers[currentBackendIndex];
+  const requestOptions = {
+      hostname: host,
+      port: port,
+      path: req.url,
+      method: req.method,
+      headers: req.headers
+  };
+  // Log incoming request details
+  console.log(`Method: ${req.method}`);
+  console.log(`URL: ${req.url}`);
+  console.log(`Headers: ${JSON.stringify(req.headers)}`);
+  console.log('Payload:', req.body);
 
-    // Log which backend server the request is being sent to
-    console.log(`Proxying request to backend server ${host}:${port}`);
+  // Log which backend server the request is being sent to
+  console.log(`Proxying request to backend server ${host}:${port}`);
 
   // Proxy the request to the selected backend server
   const proxyReq = http.request(requestOptions, (proxyRes) => {
