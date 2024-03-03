f45249332d6b35a26cc9da7df6c8cc65f3d3c8e7
Tyler Tran <tylertran1017@gmail.com>
2024-03-03 03:04:18 -0700

    Add socket support

--- backend/process/src/index.js
+++ backend/process/src/index.js
@@ -1,27 +1,82 @@
 import admin from "firebase-admin";
 import express from "express";
 import cors from "cors";
-import serviceAccount from "./seng559-firebase-adminsdk-tddx2-61de37120d.json" assert { type: "json" };
+import http from "http";
+import serviceAccount from "./seng559-firebase-adminsdk-tddx2-cbed457917.json" assert { type: "json" };
+import { Server } from "socket.io";
+import { nanoid } from "nanoid"
 
+const URL = "http://localhost";
 const PORT = process.env.PORT || 3001;
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+const server = http.createServer(app);
+const io = new Server(server,{
+  cors: {
+    origin: "*"
+  }
+});
 
 admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 const db = admin.firestore();
 
-app.post("/add", async (req, res) => {
-  const data = req.body;
-  console.log(data);
-  const docRef = await db.collection("data").add(data);
-  res.status(201).send(`Added ${data} to FireStore with ID ${docRef.id}`);
-});
+const games = {};
 
 app.get("/", (_, res) => {
-  res.status(200).send("Hello world!");
+  res.status(200).send("hello world");
+})
+
+app.get("/create", (_, res) => {
+  const id = nanoid(4);
+  const newGame = {
+    url: `${URL}:${PORT}`,
+    id: id,
+    users: []
+  };
+
+  games[id] = newGame;
+  res.status(200).json(newGame);
 });
 
-app.listen(PORT, () => {
+// temporary function to facilitate joining room
+app.post("/join", (req, res) => {
+  const { id } = req.body;
+  if(id in games){
+    res.status(200).json(games[id]);
+  } else {
+    res.status(404).send("Cannot find room");
+  }
+})
+
+io.on("connection", (socket) => {  
+  socket.on("joinGame", async (args) => {
+    const {id, username} = args;
+    games[id].users.push(username);
+    console.log(`${username} joined room ${id}`);
+
+    socket.join(id);
+    io.to(id).emit("userUpdate", games[id].users);
+
+    const sockets = await io.in(id).allSockets();
+    const socketIds = Array.from(sockets);
+    console.log(`Sockets in ${id}:`, socketIds);
+    return socketIds; // This is an array of socket IDs
+  })
+  
+  socket.on("message", async (args) => {
+    const {id, message} = args;
+    console.log(args);
+    io.to(id).emit("message", message);
+
+    const sockets = await io.in(id).allSockets();
+    const socketIds = Array.from(sockets);
+    console.log(`Sockets in ${id}:`, socketIds);
+    return socketIds; // This is an array of socket IDs
+  })
+});
+
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
