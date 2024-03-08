e2f9940df93248d5a5641a08b2691478f62c669e
Tyler <tylertran1017@gmail.com>
2024-03-08 03:55:45 -0700

    Integrate branch with new LB

--- backend/process/src/http/routes.js
+++ backend/process/src/http/routes.js
@@ -20,22 +20,11 @@ router.get("/create", (_, res) => {
   res.status(200).json(newGame);
 });
 
-router.post("/join", (req, res) => {
-  const { roomID } = req.body;
-  if (roomID in games) {
-    res.status(200).json(games[roomID]);
-  } else {
-    res.status(404).send("Cannot find room");
-  }
-});
-
 router.get("/health", (_, res) => {
-  
-  if(games.length){
-    res.status(200).json({games: games.length});
-  }
-  else{
-    res.status(200).json({games: 0});
+  if (games.length) {
+    res.status(200).json({ games: games.length });
+  } else {
+    res.status(200).json({ games: 0 });
   }
 });
 
