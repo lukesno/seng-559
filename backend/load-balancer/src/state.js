62b707def849aca54c335f824597b27e4cf07754
ivanlou <ivanloutompong@yahoo.ca>
2024-03-15 14:14:07 -0600

    OpenAI integration

--- backend/load-balancer/src/state.js
+++ backend/load-balancer/src/state.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 8080;
 // List of backend servers
 const BACKENDS_URL = [
   { host: "localhost", port: 3001 },
-  { host: "localhost", port: 3002 },
+  //{ host: "localhost", port: 3002 },
   // { host: "localhost", port: 3003 }
 ];
 
